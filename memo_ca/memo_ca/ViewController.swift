ecbff8996c711b0772d2717be2bea32c4f728010
Atsumi Ishii <atsumi.ishii74@gmail.com>
2017-02-10 11:32:36 +0800

    memo_ca

---
+++ memo_ca/memo_ca/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  memo_ca
+//
+//  Created by 石井篤美 on 2017/02/10.
+//  Copyright © 2017年 石井篤美. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ memo_ca/memo_caTests/memo_caTests.swift
@@ -0,0 +1,36 @@
+//
+//  memo_caTests.swift
+//  memo_caTests
+//
+//  Created by 石井篤美 on 2017/02/10.
+//  Copyright © 2017年 石井篤美. All rights reserved.
+//
+
+import XCTest
+@testable import memo_ca
+
+class memo_caTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
