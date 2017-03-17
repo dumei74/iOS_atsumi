d433e8cb3254283e0e72efe3f56add432ebc2629
Atsumi Ishii <atsumi.ishii74@gmail.com>
2017-03-17 10:01:47 +0800

    0316エラー解決後

--- memo_ca/memo_ca/fileBoxViewController.swift
+++ memo_ca/memo_ca/fileBoxViewController.swift
@@ -119,22 +119,8 @@ class fileBoxViewController: UIViewController,UITableViewDelegate,UITableViewDat
         performSegue(withIdentifier: "showSecondView", sender: nil)
     }
     
-    
-    
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- memo_ca/memo_ca/memoryViewController.swift
+++ memo_ca/memo_ca/memoryViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
 class memoryViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
     var scSelectedIndex = ""
     var scSelectedIndexNumber = -1
+    var indexList:[NSDictionary]! = []
 
     @IBOutlet weak var fileName: UILabel!
     @IBOutlet weak var surface: UILabel!
@@ -19,31 +21,92 @@ class memoryViewController: UIViewController,UIImagePickerControllerDelegate,UIN
     @IBOutlet weak var picture: UIImageView!
     @IBOutlet weak var word: UITextView!
     @IBOutlet weak var tapHide: UIButton!
-    
-    
 
-    
+//    indexList.add(front.text)
+//
+//    // CoreDataの内容を持ってきた
+//    indexList.add(["front":front.txt,"frontP":frontP.image,"back":back.txt,"backP":back.image])
+//    
+//    indexList = [["front","frontP","back","backP"],["front","frontP","back","backP"]]
+//    
+//
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+//        indexList = ["front","frontP","back","backP"]
+//        var myDedault = UserDefaults.standard
+//        
+//        var appDomain:Dictionary = Bundle.main.bundleIdentifier!
+//
+//        // word
+////        if scSelectedIndex != ""{
+////            word.text = scSelectedIndex
+//        if (myDefault.object(forKey: "indexList") != nil){
+//            
+//            // 「取り出した結果がnilじゃなかったら、」
+//            // データを呼び出して
+//            indexList = NSMutableArray(array:myDefault.object(forKey: "indexList") as! NSMutableArray)
+//        }
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         print ("scSelectedIndexNumber:\(scSelectedIndexNumber)")
+        read()
+        
+        
         
         // fileName
         if scSelectedIndex != ""{
             fileName.text = scSelectedIndex
+            surface.text = "front"
+        } else if scSelectedIndex != ""{
+            surface.text = "back"
         }
-
-//        // word
-//        if scSelectedIndex != ""{
-//            word.text = scSelectedIndex
-//        }
     }
-    
-    
+
+    // 読み取り
+    func read() {
+        let appDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        let viewContent = appDelegate.persistentContainer.viewContext
+        let query: NSFetchRequest<Index> = Index.fetchRequest()
+        
+        
+        let namePredicte = NSPredicate(format: "fileName = %@", fileName.text as! CVarArg)
+        query.predicate = namePredicte
+        do {
+            // 一括取得
+            let fetchResuls = try viewContent.fetch(query)
+            indexList = []
+            for result: AnyObject in fetchResuls {
+                
+                
+                
+                
+                let front: String? = result.value(forKey: "front") as? String
+                
+                
+                
+                if front != nil {
+                    //保存するディクショナリーを作成
+                    var dic: NSDictionary? = ["front":result.value(forKey: "frontP"),"frontP":result.value(forKey: "frontP"),"back":result.value(forKey: "back"),"backP":result.value(forKey: "backP")]
+                    
+                    // ディクショナリーを配列に追加
+                    
+                    indexList.append(dic!)
+                
+                }
+            }
+        } catch {
+        }
+    }
+
     
     @IBAction func tapTurn(_ sender: UITapGestureRecognizer) {
         print ("ターン")
     }
-    
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
